01207d1a8fa1bfa56d1d3d4852659c95be8f3c41
Valeriano A.R. <valen.alfonso@gmail.com>
2016-06-23 23:05:36 +0200

    Code cleanup

--- ImageCrusher/Program.cs
+++ ImageCrusher/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace ImageCrusher
 {
